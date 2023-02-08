11e436fa2ee4131d89083c038c623f9dace9ffe5
Kauanne <contatokahg01@gmail.com>
2023-02-08 11:06:46 -0300

    Script Básico

---
+++ iac0.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt senha1234) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt senha1234) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt senha1234) -G GRP_ADM
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt senha1234) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt senha1234) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt senha1234) -G GRP_VEN
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt senha1234)-G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt senha1234) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt senha1234) -G GRP_SEC
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+
+
